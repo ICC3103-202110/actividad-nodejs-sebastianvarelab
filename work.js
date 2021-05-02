7b6adefa2a85a2daa64e8f69d48bb36d8d098fb8
sebastianvarelab <svarela@miuandes.cl>
2021-05-02 17:45:27 -0400

    fourth_commit

--- work.js
+++ work.js
@@ -9,7 +9,9 @@ function fibonacci(number){
         return 1;
     }
 }
+console.log("Hellow! today we will do fibonacci")
 var prompt = require ("prompt-sync")();
-var number= prompt("please enter a number")
+var number= prompt("please enter a number: ")
 console.log("The result of your fibonacci number is:")
 console.log(fibonacci(number))
+console.log("if you want to know more numbers please run de code again")
